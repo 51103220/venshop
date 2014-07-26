322ede14e04957fee4acc1cc757598513a32d11c
Dedorewan <rewandedo1009@gmail.com>
2014-07-26 21:00:49 +0700

    Add to home some cartegory menu

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,6 @@
 class PageController < ApplicationController
   def home
+    @carts = Cart.all
   end
 
   def cart
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   resources :carts
+  get 'carts/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
